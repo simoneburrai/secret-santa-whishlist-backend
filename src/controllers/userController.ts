0569c7d1739133eef199388a92d3aded25260897
simoneburrai <burrai.s98@gmail.com>
2026-01-08 20:11:17 +0100

    feat: defining userController and Router

---
+++ src/controllers/userController.ts
@@ -0,0 +1,14 @@
+import { Response, Request } from "express";
+
+function login(req: Request, res: Response){
+
+}
+
+function register(req: Request, res: Response){
+
+}
+
+export {
+    login,
+    register
+}
---
+++ src/routers/userRouter.ts
@@ -0,0 +1,8 @@
+import { login, register } from "../controllers/userController";
+
+import { Router } from "express";
+
+
+const userRouter = Router();
+
+
