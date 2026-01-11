d2abe4a226859d765e16c83e288914aefcb7d94c
simoneburrai <burrai.s98@gmail.com>
2026-01-11 18:06:57 +0100

    feat: defining wishlist, ,gifts and priority types

--- src/types.ts
+++ src/types.ts
@@ -4,6 +4,30 @@ interface User {
     password: string
 }
 
-export type {
-    User
+type Priority  = 1 | 2 | 3 | 4 | 5;
+interface Gift {
+    name: string,
+    image?: string,
+    link?: string,
+    price: number,
+    priority: Priority
+    notes?: string
 }
+interface Wishlist {
+    userId: number,
+    name: string,
+    gifts: Gift[]
+}
+
+
+
+export type {
+    User,
+    Wishlist,
+    Gift
+}
+
+
+
+
+
